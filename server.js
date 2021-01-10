591408386efc2a5734be74176c8beaa9380c51b0
Daniel Martinez <xdanielmtz@gmail.com>
2021-01-10 14:45:46 -0500

    modifications in the serverjs

--- server.js
+++ server.js
@@ -1,40 +1,25 @@
-const path = require("path");
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require(`express`);
+const logger = require(`morgan`);
+const mongoose = require(`mongoose`);
+const htmlRoutes = require(`./routes/views`);
+const apiRoutes = require(`./routes/api`);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-// const User = require("./userModel.js");
 const app = express();
 
-app.use(logger("dev"));
-
+app.use(logger(`dev`));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(`public`));
 
-app.use(express.static("Develop/public"));
+htmlRoutes(app);
+apiRoutes(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
   useFindAndModify: false,
-  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
-
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "Develop/public/exercise.html"));
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "Develop/public/index.html"));
-});
-
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "Develop/public/stats.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
